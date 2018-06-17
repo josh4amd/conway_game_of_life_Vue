83d5731ac2628ce1b27e5e99677f6c11dd07dd7d
Josh Hekimian <josh.hekimian@jhekimian-macpro.local>
2018-06-16 21:25:12 -0400

    initial working commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <HelloWorld/> -->
+    <ConwayGrid/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import ConwayGrid from './components/ConwayGrid'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ConwayGrid
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
